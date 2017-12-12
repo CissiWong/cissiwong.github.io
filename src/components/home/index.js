720371517af9ea365bf9e0dcfd48d5d4995362ec
Cecilia Wong <cissi.wong@gmail.com>
2017-12-12 16:46:06 +0100

    moved footer to component, added social media

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./style.css"
 import Project from "../project"
 import Navigation from "../navigation"
+import Footer from "../footer"
 
 class Home extends React.Component {
 
@@ -12,6 +13,14 @@ class Home extends React.Component {
         <header className="header">
           <div className="title-one"><h1>Hello</h1></div>
           <div className="title-two"><h1>World!</h1></div>
+          <div className="social-icons">
+            <a className="github" href="https://github.com/CissiWong" alt="link to github">
+              <img src="./icons/github-logo.svg" alt="github logo" />
+            </a>
+            <a className="linkedin" href="https://www.linkedin.com/in/cecilia-wong-3446b346/" alt="link to linkedin">
+              <img src="./icons/linkedin-button.svg" alt="linkedin logo" />
+            </a>
+          </div>
         </header>
 
         <section className="portfolio-wrapper">
@@ -24,11 +33,12 @@ class Home extends React.Component {
           </div>
           <div className="portfolio-container-two">
             <Project
-              title="FAQ"
-              objective="expanding the view by toggeling classes in JavaScript"
+              title="Christmas Calendar"
+              objective="group project creating a Christmas Calendar for Kivra"
               keyword="Html, CSS, JavaScript"
-              image="images/FAQ.png" />
+              image="images/group-calendar.png" />
           </div>
+
           <div className="portfolio-container-three">
             <Project
               title="Clock"
@@ -36,31 +46,17 @@ class Home extends React.Component {
               keyword="API, JavaScript, react"
               image="images/clock.png" />
           </div>
+
           <div className="portfolio-container-four">
             <Project
-              title="Christmas Calendar"
-              objective="group project creating a Christmas Calendar for Kivra"
+              title="FAQ"
+              objective="expanding the view by toggeling classes in JavaScript"
               keyword="Html, CSS, JavaScript"
-              image="images/group-calendar.png" />
+              image="images/FAQ.png" />
           </div>
+
         </section>
-
-        <footer className="footer">
-          <div className="contact">
-            <h4>Cecilia Wong</h4>
-            <p>cissi.wong@gmail.com</p>
-            <p>+46(0)762009902</p>
-          </div>
-
-          <div className="social-media">
-            <a href="https://www.instagram.com/cissiwong/" target="_none">
-              <img src="" alt="instagram icon" />
-            </a>
-            <a href="https://www.linkedin.com/feed/" target="_none">
-              <img src="" alt="linkedin icon" />
-            </a>
-          </div>
-        </footer>
+        <Footer />
       </div>
     )
   }
