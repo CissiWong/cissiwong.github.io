a83a8955f174b84ad0b75e7430b805c91738d312
Cecilia Wong <cissi.wong@gmail.com>
2017-12-25 07:55:07 +0100

    added and aligned center grid

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import "./style.css"
 import Header from "../header"
-import Project from "../project"
 import Footer from "../footer"
 
 class Home extends React.Component {
@@ -10,38 +9,13 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <Header />
-        <section className="portfolio-wrapper">
-          <div className="portfolio-container-one wide">
-            <Project
-              title="Radio"
-              objective="fetching API's and setting state in react"
-              keyword="API, JavaScript, react"
-              image="images/radio.jpg" />
-          </div>
-          <div className="portfolio-container-two tall wide">
-            <Project
-              title="Christmas Calendar"
-              objective="group project creating a Christmas Calendar for Kivra"
-              keyword="Html, CSS, JavaScript"
-              image="images/group-calendar.png" />
-          </div>
-
-          <div className="portfolio-container-three wide">
-            <Project
-              title="Clock"
-              objective="Creating a clock using react"
-              keyword="API, JavaScript, react"
-              image="images/clock.png" />
-          </div>
-
-          <div className="portfolio-container-four wide">
-            <Project
-              title="FAQ"
-              objective="expanding the view by toggeling classes in JavaScript"
-              keyword="Html, CSS, JavaScript"
-              image="images/FAQ.png" />
-          </div>
-        </section>
+        <main className="portfolio-wrapper">
+          <div className="one tall wide" />
+          <div className="two" />
+          <div className="three">3</div>
+          <div className="four">4</div>
+          <div className="five">5</div>
+        </main>
         <Footer />
       </div>
     )
