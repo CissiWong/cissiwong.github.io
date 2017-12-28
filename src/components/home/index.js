dcd197a0dd85b8284cc24d41f1f8f67d246d086b
Cecilia Wong <cissi.wong@gmail.com>
2017-12-28 09:58:48 +0100

    moved image folder to src

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import grid from "images/CSS_grid.png"
 import "./style.css"
 import Header from "../header"
 import Footer from "../footer"
@@ -10,11 +11,13 @@ class Home extends React.Component {
       <div className="Home">
         <Header />
         <main className="portfolio-wrapper">
-          <div className="one wide tall" />
+
+          <div className="one" style={{ backgroundImage: `url(${grid})` }} />
           <div className="two tall" />
           <div className="three" />
           <div className="four" />
           <div className="five" />
+
         </main>
         <Footer />
       </div>
