bbd1cfdf3b86456540d2c9d83107f09b76a69828
Cecilia Wong <cissi.wong@gmail.com>
2017-12-28 17:32:11 +0100

    added animation, moved icons to footer

--- code/src/components/footer/index.js
+++ code/src/components/footer/index.js
@@ -10,6 +10,14 @@ class Footer extends React.Component {
           <h4>Cecilia Wong</h4>
           <a className="email" href="mailto:cissi.wong@gmail.com">cissi.wong@gmail.com</a>
           <a className="phone" href="callto:+46762009902">+46(0)762009902</a>
+          <div className="social-icons">
+            <a className="github" href="https://github.com/CissiWong" alt="link to github">
+              <img src="./icons/github-logo.svg" alt="github logo" />
+            </a>
+            <a className="linkedin" href="https://www.linkedin.com/in/cecilia-wong-3446b346/" alt="link to linkedin">
+              <img src="./icons/linkedin-button.svg" alt="linkedin logo" />
+            </a>
+          </div>
         </div>
       </footer>
     )
--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -3,21 +3,15 @@ import "./style.css"
 import Navigation from "../navigation"
 
 export default class Header extends React.Component {
+
   render() {
     return (
-      <header className="header">
-        <Navigation />
-        <div className="title-one"><h1>Hello</h1></div>
-        <div className="title-two"><h1>World</h1></div>
-        <div className="social-icons">
-          <a className="github" href="https://github.com/CissiWong" alt="link to github">
-            <img src="./icons/github-logo.svg" alt="github logo" />
-          </a>
-          <a className="linkedin" href="https://www.linkedin.com/in/cecilia-wong-3446b346/" alt="link to linkedin">
-            <img src="./icons/linkedin-button.svg" alt="linkedin logo" />
-          </a>
-        </div>
-      </header>
+      <div className="header-overlay">
+        <header className="header">
+          <span className="logo" />
+          <Navigation />
+        </header>
+      </div>
     )
   }
 }
