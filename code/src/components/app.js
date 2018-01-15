826c0bcbbd228192ba203bd651f84451e68182d6
Cecilia Wong <cissi.wong@gmail.com>
2018-01-15 11:13:14 +0100

    changed to hash router

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
+// import { BrowserRouter, Route } from "react-router-dom"
+import { HashRouter, Route } from "react-router-dom"
 
 import HomeView from "./homeview"
 import About from "./about"
-import Contact from "./contact"
+// import Contact from "./contact"
 // import ProjectPreview from "./project-preview"
 import ProjectView from "./projectview"
 
@@ -11,14 +12,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <div>
           <Route path="/" exact component={HomeView} />
           <Route path="/about" component={About} />
           {/* <Route path="/contact" component={Contact} /> */}
           <Route path="/projects/:id" component={ProjectView} />
         </div>
-      </BrowserRouter>
+      </HashRouter>
 
     )
   }
